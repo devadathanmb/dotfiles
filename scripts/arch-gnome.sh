34249508b941b795294f02ac41d00049af51fb21
Devadathan M B <devadathanmb@gmail.com>
2022-10-14 22:49:47 +0530

    Added script for arch installation

---
+++ scripts/arch-gnome.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# Install yay
+clear
+echo "Instlling yay"
+pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+# Install packages
+clear
+pacman -S --noconfirm firefox code btop neofetch vlc mpv gnome-tweaks gnome-themes-extra transmission-gtk zsh simplescreenrecorder bat neovim gparted cmatrix jdk-openjdk flatpak nodejs lsd man alacritty kitty npm libreoffice-still
+
+ yay --noconfirm -S discord
+ yay --noconfirm -S visual-studio-code-bin  
+ yay --noconfirm -S google-chrome 
+ yay --noconfirm -S brave-bin 
+ yay --noconfirm -S telegram-desktop-bin 
+ yay --noconfirm -S ventoy-bin 
+ yay --noconfirm -S discord
+ yay --noconfirm -S onlyoffice-bin
