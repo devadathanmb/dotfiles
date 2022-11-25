c3fa5b3e0f12a25935edcb43b1e471818e2f99a0
Devadathan M B <devadathanmb@gmail.com>
2022-11-25 14:08:24 +0530

    Update dashboard stuff

--- nvim/lua/njan/nvim-dashboard.lua
+++ nvim/lua/njan/nvim-dashboard.lua
@@ -29,7 +29,7 @@ db.custom_center = {
 
 }
 
-db.custom_footer  = {"Happy hacking!"}
+db.custom_footer  = {desc = "   Happy Hacking!   ", fg = "#F06292"}
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "dashboard",
