113a722207fef0b284c20d161507ab14651691e1
Devadathan M B <devadathanmb@gmail.com>
2022-11-25 19:59:10 +0530

    Configure flutter-tools properly

--- nvim/lua/njan/flutter-tools.lua
+++ nvim/lua/njan/flutter-tools.lua
@@ -1,6 +1,11 @@
 local status_ok, flutter_tools = pcall(require, "flutter-tools")
 if not status_ok then
-    vim.notify("Flutter tools not found")
+	vim.notify("Flutter tools not found")
 end
 
-flutter_tools.setup{}
+flutter_tools.setup({
+	flutter_path = "/opt/flutter/bin/flutter",
+	lsp = {
+		on_attach = require("njan.lsp.handlers").on_attach,
+	},
+})
