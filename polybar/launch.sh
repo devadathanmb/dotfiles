88ce82a2e09dd2ef2c9c065c701a05aee22a529e
Devadathan M B <devadathanmb@gmail.com>
2022-12-07 19:31:50 +0530

    i3 rice commit 3

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-killall -q polybar
+dir="$HOME/.config/polybar"
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar | tee -a /tmp/polybar1.log & disown
-# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+launch_bar() {
+	# Terminate already running bar instances
+	killall -q polybar
 
-echo "Bars launched..."
+	# Wait until the processes have been shut down
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+	# Launch the bar
+    polybar -q main -c "$dir/$style/config.ini" &	
+}
+
+style="gruvbox"
+launch_bar
