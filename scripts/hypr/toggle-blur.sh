637745dedc474580125d9aedc1df9ae9ba08764b
Devadathan M B <devadathanmb@gmail.com>
2023-03-31 21:36:59 +0530

    Add script to toggle blur on the fly 🛸

---
+++ scripts/hypr/toggle-blur.sh
@@ -0,0 +1,44 @@
+#! /usr/bin/env bash
+
+turn_on_blur(){
+  # Change background opacity of kitty
+  sed -i 's/^background_opacity [0-9]\+\(\.[0-9]\+\)\{0,1\}$/background_opacity 0.80/' "$HOME/.config/kitty/kitty.conf"
+  
+  # Change background opacity of alacritty
+  sed -i 's/ opacity: [0-9]\+\(\.[0-9]\+\)\{0,1\}$/ opacity: 0.80/' "$HOME/.config/alacritty/alacritty.yml"
+
+  # Hyprland blur
+  hyprctl keyword decoration:blur 1;
+
+  # Reload kitty
+  pkill -USR1 kitty
+} 
+
+turn_off_blur(){
+  # Change background opacity of kitty
+  sed -i 's/^background_opacity [0-9]\+\(\.[0-9]\+\)\{0,1\}$/background_opacity 1.00/' "$HOME/.config/kitty/kitty.conf"
+  
+  # Change background opacity of alacritty
+  sed -i 's/ opacity: [0-9]\+\(\.[0-9]\+\)\{0,1\}$/ opacity: 1.00/' "$HOME/.config/alacritty/alacritty.yml"
+
+  # Hyprland blur
+  hyprctl keyword decoration:blur 0;
+
+  # Reload kitty
+  pkill -USR1 kitty
+}
+
+main() {
+  status=$(hyprctl getoption decoration:blur -j | jq ".int")
+  echo $status
+  if [[ $status -eq 0 ]]
+  then
+    notify-send "👓 Turning blur on"
+    turn_on_blur
+  else
+    notify-send "🕶️ Turning blur off"
+    turn_off_blur
+  fi
+}
+
+main
