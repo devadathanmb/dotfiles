6eb35f583e7e3e5b9c8fb20bbddbd59cc4de14b8
Devadathan M B <devadathanmb@gmail.com>
2023-03-31 22:10:20 +0530

    Add functions to go to first and last workspace

--- scripts/hypr/next-workspace.sh
+++ scripts/hypr/next-workspace.sh
@@ -6,19 +6,28 @@ last_workspace=$(hyprctl workspaces -j | jq -c 'sort_by(.id) | .[-1].id')
 next_workspace=$(( last_workspace+1 ))
 
 function next_workspace(){
-  echo "Next workspace"
   hyprctl dispatch workspace $next_workspace
 }
 
+function last(){
+  hyprctl dispatch workspace $last_workspace
+}
+
+function first(){
+  first_workspace=$(hyprctl workspaces -j | jq -c 'sort_by(.id) | .[0].id')
+  hyprctl dispatch workspace $first_workspace
+}
+
 function move_to_next_workspace(){
-  echo "Move to next workspace"
   hyprctl dispatch movetoworkspace $next_workspace
 }
 
-while getopts nm flag
+while getopts nmlf flag
 do
     case "${flag}" in
       n) next_workspace ;;
       m) move_to_next_workspace ;;
+      l) last ;;
+      f) first ;;
     esac
 done
