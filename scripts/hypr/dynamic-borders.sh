774eb60a15dbc112d0590d49e70c35800c75c731
Devadathan M B <devadathanmb@gmail.com>
2023-04-05 12:42:21 +0530

    refactor dynamic borders script

--- scripts/hypr/dynamic-borders.sh
+++ scripts/hypr/dynamic-borders.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 function handle {
-    if [[ ${1:0:10} == "openwindow" || ${1:0:10} == "movewindow" ]]; then
+    if [[ ${1:0:10} == "openwindow" ]]
+    then
         window_id=$(echo $1 | cut --delimiter ">" --fields=3 | cut --delimiter "," --fields=1)
         workspace_id=$(echo $1 | cut --delimiter ">" --fields=3 | cut --delimiter "," --fields=2)
         windows=$(hyprctl workspaces -j | jq ".[] | select(.id == $workspace_id) | .windows")
@@ -20,15 +21,36 @@ function handle {
             done
         fi
 
-      # Handle all the other workspaces with only one window
-      single_window_workspaces=$(hyprctl workspaces -j | jq '.[] | select(.windows == 1)' | jq ".id")
-      for workspace in $single_window_workspaces
-      do
-        window=$(hyprctl clients -j | jq ".[] | select(.workspace.id == $workspace) | .address")
-        hyprctl setprop address:$(echo $window | xargs) forcenoborder 1 lock
-      done
+    elif [[ ${1:0:10} == "movewindow"  ]]
+    then
+        window_id=$(echo $1 | cut --delimiter ">" --fields=3 | cut --delimiter "," --fields=1)
+        workspace_id=$(echo $1 | cut --delimiter ">" --fields=3 | cut --delimiter "," --fields=2)
+        windows=$(hyprctl workspaces -j | jq ".[] | select(.id == $workspace_id) | .windows")
 
-    elif [[ ${1:0:11} == "closewindow" ]]; then
+        if [[ $windows -eq 1 ]]
+        then
+            hyprctl setprop address:0x$window_id forcenoborder 1 lock
+        elif [[ $windows -eq 2 ]]
+        then
+            addresses=$(hyprctl clients -j | jq -r --arg foo "$foo" ".[] | select(.workspace.id == $workspace_id) | .address")
+            for address in $addresses
+            do
+                if [[ "$address" != "$window_id" ]]; then
+                    hyprctl setprop address:$(echo $address | xargs) forcenoborder 0 lock
+                fi
+            done
+        fi
+
+        # Handle all the other workspaces with only one window
+        single_window_workspaces=$(hyprctl workspaces -j | jq '.[] | select(.windows == 1)' | jq ".id")
+        for workspace in $single_window_workspaces
+        do
+            window=$(hyprctl clients -j | jq ".[] | select(.workspace.id == $workspace) | .address")
+            hyprctl setprop address:$(echo $window | xargs) forcenoborder 1 lock
+        done
+
+    elif [[ ${1:0:11} == "closewindow" ]]
+    then
         workspace_id=$(hyprctl activewindow -j | jq ".workspace.id")
         windows=$(hyprctl workspaces -j | jq ".[] | select(.id == $workspace_id) | .windows")
 
