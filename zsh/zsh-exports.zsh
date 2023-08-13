657266d5e7fc055c44f2f3a9214541f3e66bab43
Devadathan M B <devadathanmb@gmail.com>
2023-08-14 00:59:19 +0530

    update xexports

--- zsh/zsh-exports.zsh
+++ zsh/zsh-exports.zsh
@@ -12,14 +12,14 @@ export PATH="$HOME/.nvm/versions/node/v20.3.1/bin/":$PATH
 # Bat cat theme
 export BAT_THEME="Visual Studio Dark+"
 
-# Flutter 
-if [[ -e "/opt/flutter" ]]
-then
-    export PATH=$PATH:"/opt/flutter/bin"
-fi
-
-# Spicetify
-if [[ -e "$HOME/.spicetify/" ]]
-then
-    export PATH=$PATH:"$HOME/.spicetify/"
-fi
+# # Flutter 
+# if [[ -e "/opt/flutter" ]]
+# then
+#     export PATH=$PATH:"/opt/flutter/bin"
+# fi
+#
+# # Spicetify
+# if [[ -e "$HOME/.spicetify/" ]]
+# then
+#     export PATH=$PATH:"$HOME/.spicetify/"
+# fi
